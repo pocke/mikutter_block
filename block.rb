f584d5ed703835bb22a5d67ed6fdc1ded257be31
pocke <p.ck.t22@gmail.com>
2014-01-29 16:13:49 +0900

    昔書いたコードがきもかった

--- block.rb
+++ block.rb
@@ -1,18 +1,20 @@
 #-*- coding: utf-8 -*-
 
-# Copyright 2013, pocket
+# Copyright 2013-2014, pocke
 # Licensed MIT
 # http://opensource.org/licenses/mit-license.php
 
 Plugin.create(:block) do
-  command(:block,
+  command(
+    :block,
     name: 'block',
-    description: '(ﾟ∀ﾟ)o彡゜ブロック!ブロック!',
+    description: 'ブロック',
     condition: lambda{|x| true},
     visible: true,
-    role: :timeline) do |opt|
+    role: :timeline
+  ) do |opt|
     opt.messages.each do |m|
-      (Service.primary.twitter/'blocks/create').json(:screen_name => m.user.idname)
+      (Service.primary.twitter/'blocks/create').json(screen_name: m.user.idname)
     end
   end
 end
